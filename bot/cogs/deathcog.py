c3dd70a067935ba8df4da9e70c744c8a62779170
WylieGlover <rg11445@georgiasouthern.edu>
2023-06-08 00:28:30 +0000

    Upload new files

--- bot/cogs/deathcog.py
+++ bot/cogs/deathcog.py
@@ -6,12 +6,17 @@ class DeathCog(commands.Cog):
         self.bot = bot
         self.death = Death()
     
-    @commands.command(name="death")
+    @commands.command(name="death", aliases=["d"])
     async def incrementdeaths(self, ctx):
         death_count = self.death.increment_death_count(ctx.channel)
+        await ctx.send(f'Death counter: {death_count}')
+
+    @commands.command(name="deaths", aliases=["deathcounter", "deathcount"])
+    async def displaydeaths(self, ctx):
+        death_count = self.death.get_death_count(ctx.channel)
         await ctx.send(f"Death counter: {death_count}")
 
-    @commands.command(name='resetdeaths')
+    @commands.command(name="resetdeaths", aliases=["resetdeath", "resetd", "rd"])
     async def resetdeaths(self, ctx):
         if ctx.author.is_mod:
             death_count = self.death.reset_death_count(ctx.channel)
--- bot/cogs/winlosscog.py
+++ bot/cogs/winlosscog.py
@@ -6,27 +6,31 @@ class WinLossCog(commands.Cog):
         self.bot = bot
         self.win_loss = WinLoss()
     
-    @commands.command(name="win")
+    @commands.command(name="win", aliases=["w"])
     async def incrementwins(self, ctx):
         win_count = self.win_loss.increment_win_count(ctx.channel)
         await ctx.send(f"Wins: {win_count}")
 
-    @commands.command(name="loss")
+    @commands.command(name="loss", aliases=["l"])
     async def incrementloss(self, ctx):
         loss_count = self.win_loss.increment_loss_count(ctx.channel)
         await ctx.send(f"Losses: {loss_count}")
 
-    @commands.command(name="wl")
+    @commands.command(name="wl", aliases=["winloss", "wins", "losses", "WL"])
     async def displaywl(self, ctx):
         win_count = self.win_loss.get_win_count(ctx.channel)
         loss_count = self.win_loss.get_loss_count(ctx.channel)
-        await ctx.send(f"Wins: {win_count} | Losses: {loss_count}")
+
+        ratio = 0
+        if(win_count != 0):
+            ratio = round(win_count / (win_count + loss_count), 2)
+        await ctx.send(f"{win_count}W  {loss_count}L | {ratio}")
     
-    @commands.command(name="resetwl")
+    @commands.command(name="resetwl", aliases=["rwl"])
     async def resetwinloss(self, ctx):
         if ctx.author.is_mod:
             win_count, loss_count = self.win_loss.reset_win_loss_count(ctx.channel)
-            await ctx.send(f"Win: {win_count} | Losses: {loss_count}")
+            await ctx.send(f"W: {win_count} | L: {loss_count}")
 
 def prepare(bot):
     bot.add_cog(WinLossCog(bot))

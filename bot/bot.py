38702120a46cb31577796366849b8fa87dd36dfc
WylieGlover <rg11445@georgiasouthern.edu>
2023-06-09 11:40:52 -0400

    Update DB

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from death_count import Death
 from twitchio.ext import commands, routines
 from tinydb import TinyDB, Query
@@ -6,26 +6,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-cogs = [
-    "cogs.deathcog",
-    "cogs.winlosscog"
-]
+cogs = ["cogs.deathcog", "cogs.winlosscog"]
+
 
 class Bot(commands.Bot):
     def __init__(self):
-        self.db = TinyDB('/home/wylie/DB/db.json', sort_keys=True, indent=4, separators=(',', ': '))
-        self.death_table = self.db.table('death_counter_table')
-        self.win_loss_table = self.db.table('win_loss_counter_table')
+        self.db = TinyDB(
+            "../DB/db.json", sort_keys=True, indent=4, separators=(",", ": ")
+        )
+        self.death_table = self.db.table("death_counter_table")
+        self.win_loss_table = self.db.table("win_loss_counter_table")
 
         self.channels = os.getenv("CHANNELS").split("#")
 
         super().__init__(
-            token=os.getenv('TMI_TOKEN'),
-            
-            client_id=os.getenv('CLIENT_ID'),
-            nick=os.getenv('BOT_NICK'),
-            prefix=os.getenv('BOT_PREFIX'),
-            initial_channels=self.channels
+            token=os.getenv("TMI_TOKEN"),
+            client_id=os.getenv("CLIENT_ID"),
+            nick=os.getenv("BOT_NICK"),
+            prefix=os.getenv("BOT_PREFIX"),
+            initial_channels=self.channels,
         )
 
         for cog in cogs:
@@ -37,19 +36,22 @@ class Bot(commands.Bot):
             death_table_users = self.death_table.search(User.name == channel)
             if not death_table_users:
                 self.death_table.insert({"name": channel, "death_count": 0})
-            
+
             win_loss_table_users = self.win_loss_table.search(User.name == channel)
             if not win_loss_table_users:
-                self.win_loss_table.insert({"name": channel, "win count": 0, "loss count": 0})
-    
+                self.win_loss_table.insert(
+                    {"name": channel, "win count": 0, "loss count": 0}
+                )
+
     async def event_ready(self):
         print(f"Logged in as | {self.nick}")
-        print(f'Logged into {self.channels}')
+        print(f"Logged into {self.channels}")
         self.initialize_db(self.channels)
 
     @commands.command(name="ping")
     async def ping(self, ctx):
         await ctx.send("pong")
+
 
 if __name__ == "__main__":
     bot = Bot()
--- bot/death_count.py
+++ bot/death_count.py
@@ -1,13 +1,18 @@
 from tinydb import TinyDB, Query
 
-class Death():
+
+class Death:
     def __init__(self):
-        self.db = TinyDB('/home/wylie/DB/db.json', sort_keys=True, indent=4, separators=(',', ': '))
-        self.death_table = self.db.table('death_counter_table')
+        self.db = TinyDB(
+            "../DB/db.json", sort_keys=True, indent=4, separators=(",", ": ")
+        )
+        self.death_table = self.db.table("death_counter_table")
 
     def get_death_count(self, user) -> int:
         User = Query()
-        death_count = self.death_table.search(User.name == user.name)[0].get("death_count", 0)
+        death_count = self.death_table.search(User.name == user.name)[0].get(
+            "death_count", 0
+        )
         return death_count
 
     def increment_death_count(self, user) -> int:
@@ -15,7 +20,7 @@ class Death():
         death_count += 1
         self.death_table.update({"death_count": death_count}, Query().name == user.name)
         return death_count
-        
+
     def reset_death_count(self, user) -> int:
         death_count = self.get_death_count(user)
         death_count = 0
